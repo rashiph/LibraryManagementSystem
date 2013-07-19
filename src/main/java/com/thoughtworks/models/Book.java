043d7b36b3d012820593381ffc37b4a8815f605d
Rashi Phuloria <rashiph@thoughtworks.com>
2013-07-19 17:26:20 +0530

    book

--- src/main/java/com/thoughtworks/models/Book.java
+++ src/main/java/com/thoughtworks/models/Book.java
@@ -1,19 +1,12 @@
 package com.thoughtworks.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
 
 @Entity
 @Table(name = "BOOKS")
-@NoArgsConstructor
-@AllArgsConstructor
 public class Book {
 
     @Id

cea656406e7cb3dab9e7a4acd5735303616fae4e
Megha Agarwal <agarwal.megha7@gmail.com>
2013-07-19 19:39:40 +0530

    made the flow to add book

--- src/main/java/com/thoughtworks/repositories/BookRepository.java
+++ src/main/java/com/thoughtworks/repositories/BookRepository.java
@@ -3,12 +3,10 @@ package com.thoughtworks.repositories;
 import com.thoughtworks.models.Book;
 import com.thoughtworks.utils.HibernateUtil;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
-
-import javax.management.Query;
 
 @Repository
 @AllArgsConstructor
@@ -19,11 +17,15 @@ public class BookRepository {
     public BookRepository() {
         sessionFactory = HibernateUtil.getSessionFactory();
         session = sessionFactory.openSession();
+
     }
 
     public void add(String name) {
         Book book = new Book();
         book.setName(name);
         session.save("books", book);
+        Transaction tx = session.beginTransaction();
+        tx.commit();
+
     }
 }

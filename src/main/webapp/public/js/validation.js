98e3f545c88751a1c5c84c3c40ef11eb38b692df
Vishal Sharma <vishalsh@thoughtworks.com>
2013-08-01 21:14:35 +0530

    Added some css to add user page

--- src/main/webapp/public/js/validation.js
+++ src/main/webapp/public/js/validation.js
@@ -1,42 +1,34 @@
 function validate()
 {
-   if(document.bookForm.name.value=="" )
+   if(document.bookForm.bookName.value=="" )
    {
      alert( "Please provide your name !" );
-
-
      return false;
    }
-   var author = document.bookForm.author.value;
+   var author = document.bookForm.authorName.value;
    if(isNaN(author)==false || author== "")
    {
      alert( "Please enter valid author name !" );
-
      return false;
    }
    var category = document.bookForm.category.value;
    if(isNaN(category)==false || category== "")
    {
      alert( "Please enter valid category !" );
-
      return false;
    }
     var edition= document.bookForm.edition.value;
    if(isNaN(edition)==true ||edition=="")
    {
         alert( "Please enter valid edition !" );
-
         return false;
-    }
+   }
    var price = document.bookForm.price.value;
    if(isNaN(price)==true || price== "")
    {
      alert( "Please enter valid price of book !" );
-
      return false;
    }
-
-
 }
 
  $(function() {
